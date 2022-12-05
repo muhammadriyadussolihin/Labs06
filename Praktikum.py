424653d1191e214c98d30906d29890b107684127
muhammadriyadussolihin <riyadaldehebaran@gmail.com>
2022-12-05 14:47:18 +0700

    Upload File

--- Praktikum.py
+++ Praktikum.py
@@ -1,29 +1,3 @@
-# Labs06
-# **PROGRAM INPUT NILAI MAHASISWA MENGGUNAKAN FUNGSI**
-
-```sh
-Nama   : Muhammad Riyadussolihin 
-Nim    : 312210404
-Matkul : Bahasa Pemograman
-```
-
-## **PENJELASAN**
-
-1. Deklarasi dictionary dengan nama _dataMahasiswa_ untuk menampung semua data/element.
-2. gunakan fungsi _def tambah()_ di isi dengan inputan nama, nim, tugas, uts, uas dan perhitungan nilai akhir untuk dan di masukan ke dictonary _dataMahasiswa_.
-3. gunakan fungsi _def tampilkan()_ di isi dengan cetak isi dari dictonary.
-4. gunakan fungsi _def hapus(nama)_ di isi dengan syntax delet untuk menghapus element _nama_ pada dictonary _dataMahasiswa_.
-5. gunakan fungsi _def ubah(nama)_ di isi dengan inputan _nama_ dan mengubah isi element pada _nama_ tersebut.
-6. gunakan _while True_ untuk menlooping/mengulang statment.
-7. gunakan statment _if, elif, else,_ di dalam _while True_ dan panggil fungsi dari masing masing fungsi, contoh:
-```sh
-tambah()
-tampilkan()
-hapus(nama)
-ubah(nama)
-```
-8. SELESAI
-```
 dataMahasiswa = {}
 
 print("=" * 65)
@@ -101,7 +75,3 @@ while True:
         exit()
     else:
         print("PILIHAN MENU TIDAK ADA!")
-```sh
-
-## **FLOWCHART**
-![img](gambar/flowchart.png)
